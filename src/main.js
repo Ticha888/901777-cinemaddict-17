dfd4f9de60db4bf3170e03a6637714b9fa97c075
Ivan <ticha385@gmail.com>
2022-05-08 16:50:58 +0300

    исправялем линтер и импорт

--- src/main.js
+++ src/main.js
@@ -2,7 +2,7 @@ import NewFilterView from './view/filter-view.js';
 import NewFilmsContainerView from './view/films-container-view.js';
 import NewSortCardFilmView from './view/sort-view.js';
 import NewProfileView from './view/profile-view.js';
-import createBtnShowMore from './view/btn-show-more-view.js';
+import NewBtnShowMoreView from './view/btn-show-more-view.js';
 import NewPopupView from './view/popup-view.js';
 import {render} from './render.js';
 import FilmListPresenter from './presenter/board-presenter.js';
@@ -11,7 +11,7 @@ const siteMainElement = document.querySelector('.main');
 const headerElement = document.querySelector('header');
 const filmListPresenter = new FilmListPresenter();
 
-render (new NewProfileView(),headerElement);
+render(new NewProfileView(),headerElement);
 render(new NewFilterView(), siteMainElement);
 render(new NewSortCardFilmView(), siteMainElement);
 render(new NewFilmsContainerView(),siteMainElement);
@@ -20,9 +20,9 @@ const filmsContainer = document.querySelector('.films-list__container');
 
 filmListPresenter.init(filmsContainer);
 
-const filmList = document.querySelector('.films-list')
+const filmList = document.querySelector('.films-list');
 
-render (new createBtnShowMore(), filmList);
+render (new NewBtnShowMoreView(), filmList);
 render (new NewPopupView(),document.body);
 
 
